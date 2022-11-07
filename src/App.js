b9cabbe7b1ac77ffb39ab04dd02b10c50aad307a
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-07 22:22:13 +0200

    fetch added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
@@ -8,9 +8,11 @@ import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com?apikey=5f524fec";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
+    setMovies(data.Search);
   };
   useEffect(() => {
     searchMovies();
@@ -26,9 +28,17 @@ const App = () => {
         />
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
-      <div className="container">
-
-      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
