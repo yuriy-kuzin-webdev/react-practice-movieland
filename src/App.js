99868463261c0c504c9e3f385295d4722eea07ef
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-07 22:14:52 +0200

    movie card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
-import React from "react";
+import { useEffect } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+// 5f524fec
+
+const API_URL = "http://www.omdbapi.com?apikey=5f524fec";
+
 const App = () => {
-  return <h1>App</h1>;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+  };
+  useEffect(() => {
+    searchMovies();
+  }, []);
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+      <div className="container">
+
+      </div>
+    </div>
+  );
 };
 
 export default App;
